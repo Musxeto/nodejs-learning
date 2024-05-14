cf5fe6ba9f624b9c1b05e3821f2c8dbeefe01c5b
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-14 15:20:48 +0500

    routing & html

--- day2/app.js
+++ day2/app.js
@@ -4,5 +4,8 @@ const app = express();
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.send("<p>biganigga</p>"); // Use quotes to send HTML as a string
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
 });
