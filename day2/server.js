a42d5e37fa55396c9cdb148c9499342a31f92881
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-14 10:38:54 +0500

    html using node js

--- day2/server.js
+++ day2/server.js
@@ -1,12 +1,18 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   // Set the response header using res.setHeader
   res.setHeader("Content-Type", "text/html");
-  res.write("<p>big sleepy 223</p>");
-  res.end(); // Ensure you end the response
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
