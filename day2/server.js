537e8afc9bfb30a38fecd582851f8df490c06d26
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-14 11:13:47 +0500

    paths

--- day2/server.js
+++ day2/server.js
@@ -5,6 +5,20 @@ const server = http.createServer((req, res) => {
 
   // Set the response header using res.setHeader
   res.setHeader("Content-Type", "text/html");
+
+  let path = "./views";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
   fs.readFile("./views/index.html", (err, data) => {
     if (err) {
       console.error(err);
