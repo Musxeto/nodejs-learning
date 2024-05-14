474aa71734015eeb1607c085e3fb3a223a60ce94
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-14 11:56:39 +0500

    piss on yo grave dawg

--- day2/server.js
+++ day2/server.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
       path += "about.html";
       res.statusCode = 200;
       break;
-    case "/about-me":
+    case "/about-us":
       res.statusCode = 301;
       res.setHeader("Location", "/about");
       res.end();
