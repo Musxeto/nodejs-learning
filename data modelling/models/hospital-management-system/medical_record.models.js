338c66d6fc44d2444f47443713a5031aa527ba55
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-18 12:02:49 +0500

    medical record schema data modeliing

--- data modelling/models/hospital-management-system/medical_record.models.js
+++ data modelling/models/hospital-management-system/medical_record.models.js
@@ -1,6 +1,24 @@
 import mongoose from "mongoose";
 
-const medicalRecordSchema = new mongoose.Schema({}, { timestamps: true });
+const medicalRecordSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+    },
+    treatedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+      },
+    ],
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hospital",
+    },
+  },
+  { timestamps: true }
+);
 
 export const MedicalRecord = mongoose.model(
   "MedicalRecord",
