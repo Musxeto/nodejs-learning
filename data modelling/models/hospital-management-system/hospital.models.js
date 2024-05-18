f91d4066891562aaac2aff0d3df2c86d32b59f9f
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-18 11:57:32 +0500

    hospital modeeling

--- data modelling/models/hospital-management-system/hospital.models.js
+++ data modelling/models/hospital-management-system/hospital.models.js
@@ -1,5 +1,30 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({}, { timestamps: true });
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    addressLine1: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    pincode: {
+      type: String,
+      required: true,
+    },
+    specializedIn: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema);
