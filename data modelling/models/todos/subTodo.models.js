6bb0c652c11652d4823633bb34566bfe34bcbcd5
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-18 10:07:43 +0500

    real nigga

--- data modelling/models/todos/subTodo.models.js
+++ data modelling/models/todos/subTodo.models.js
@@ -1,4 +1,20 @@
 import mongoose from "mongoose";
-const subTodoSchema = new mongoose.Schema({}, { timestamps: true });
+const subTodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const SubTodo = mongoose.model("SubTodo", subTodoSchema);
